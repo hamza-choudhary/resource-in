3ba388e0057304d2ef82c0ea799464db356f3149
hamzach <humzach700@gmail.com>
2022-05-18 19:37:12 +0500

    schema updated

--- database_schema.sql
+++ database_schema.sql
@@ -5,18 +5,18 @@ CREATE TABLE user (
     user_name varchar(255) NOT NULL,
     user_email varchar(255) NOT NULL,
     user_password longtext NOT NULL,
-    user_first_name varchar(255) NOT NULL,
-    user_middle_name varchar(255) NOT NULL,
-    user_father_husband_name varchar(255) NOT NULL,
-    user_gender varchar(255) NOT NULL,
-    user_date_of_birth varchar(255) NOT NULL,
+    user_first_name varchar(255),
+    user_middle_name varchar(255),
+    user_father_husband_name varchar(255),
+    user_gender varchar(255),
+    user_date_of_birth DATE,
     user_marital_status BOOLEAN,
     user_religion varchar(255),
-    user_nationality varchar(255) NOT NULL,
+    user_nationality varchar(255),
     user_blood_group varchar(255),
     user_profile_pic varchar(255),
-    user_salutation varchar(20) NOT NULL,
-    user_mobile_no varchar(255) NOT NULL,
+    user_salutation varchar(20),
+    user_mobile_no varchar(255),
     user_landline_no varchar(255),
     
     PRIMARY KEY (user_id)
@@ -56,3 +56,32 @@ CREATE TABLE user_driving_card (
 
 DELETE FROM user_driving_card;
 DROP TABLE user_driving_card;
+
+
+/*============attendacne===========*/
+CREATE TABLE attendance (
+    attendance_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    check_in DATETIME,
+    check_out DATETIME,
+    
+    PRIMARY KEY (attendance_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+DELETE FROM attendance;
+DROP TABLE attendance;
+
+/*============relaxation===========*/
+CREATE TABLE relaxation (
+    relaxation_id int NOT NULL AUTO_INCREMENT,
+    attendance_id int NOT NULL,
+	relaxation_type varchar(255) NOT NULL, /*value would be of CHECK_IN or CHECK_OUT*/
+    
+    PRIMARY KEY (relaxation_id),
+    FOREIGN KEY (attendance_id) REFERENCES attendance(attendance_id)
+);
+
+DELETE FROM relaxation;
+DROP TABLE relaxation;
+
