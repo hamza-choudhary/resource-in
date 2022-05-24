af233831f2297856d2bf38fec9073c7fc257d463
hamzach <humzach700@gmail.com>
2022-05-24 23:37:06 +0500

    redefine absent logic

--- src/api/services/dashboard.js
+++ src/api/services/dashboard.js
@@ -5,25 +5,36 @@ export const empMonthlyAbsents = async (empId) => {
 	const data = rows[0]
 
 	let absents = 0
-	for (let i = 1; i < data.length; i++) {
-		const date1 = new Date(data[i - 1])
-    const date2 = new Date(data[i])
 
-    const day1 = date1.getDay()
-    const day2 = date2.getDay()
+	const firstDate = data[0].check_in.getDate()
+	const lastDate = data[data.length - 1].check_in.getDate()
+	const month = data[0].check_in.getMonth()
+	const year = data[0].check_in.getFullYear()
 
-		if (day1 === 0 || day2 === 0 || day1 === 6 || day2 === 6) {
-			continue
+	const currentMonthDates = []
+
+	//create all dates between first check in and last check in by employee
+	for (let i = firstDate; i <= lastDate; i++) {
+		currentMonthDates.push(i)
+	}
+
+	//delete days that are checked_in
+	data.forEach((value) => {
+		const checkInDate = value.check_in.getUTCDate()
+		const presentDateIndex = currentMonthDates.indexOf(checkInDate)
+
+		if (presentDateIndex > -1) {
+			currentMonthDates.splice(presentDateIndex, 1)
 		}
+	})
 
-		const diffInTime =
-			new Date(data[i].check_in) - new Date(data[i - 1].check_in)
-		const diffInDays = diffInTime / (1000 * 3600 * 24)
-
-		if (diffInDays > 1) {
+	//count absents
+	currentMonthDates.forEach((day) => {
+		const date = new Date(year, month, day + 1) //node is somehow creating date of day-1 but on browser it works fine
+		if (date.getUTCDay() !== 6 && date.getUTCDay() !== 0) {
 			absents++
 		}
-	}
+	})
 
 	return absents
 }
